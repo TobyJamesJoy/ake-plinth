fb70ed9c032afb3f85b3571194abc312249f614a
Tobias Brodel <tbrodel@utumno.bubsr.lan>
2016-11-28 18:01:32 +1100

    rm spectra sources

--- arduino/plinth/plinth.ino
+++ arduino/plinth/plinth.ino
@@ -1,7 +1,8 @@
 #include "LPD8806.h"
 #include "SPI.h"
 #include "ake_common.h"
-#include "spectra.h"
+
+
 
 LPD8806 strip = LPD8806(NUM_LEDS, DATAPIN, CLOCKPIN);
 
@@ -12,11 +13,8 @@ setup()
         strip.begin();
         // Update the strip, to start they are all 'off'
         strip.show();
-        
-        def_palette();
 
         pinMode(OUTPIN, OUTPUT);
-
         // opens serial port, sets data rate to 9600 bps
         Serial.begin(BAUD_RATE);
         Serial.setTimeout(TIMEOUT);
@@ -24,76 +22,53 @@ setup()
         
 void 
 loop() {
-        struct rgb *col = (struct rgb*)malloc(sizeof(struct rgb*));
-        int rout, lum, width, pix;
-
+        struct rgb *col = (rgb *)malloc(sizeof(rgb));
+        init_col(col);
+        int cnt, pix, width;
+        
         while (1) {
+                /* 
+                 * look for the next valid integer in the incoming 
+                 * serial stream
+                 */
                 if (Serial.available()) {
-                        /* 
-                         * look for the next valid integer in the 
-                         * incoming serial stream
-                         */
-                        rout = Serial.parseInt(); //color
-                        check_int(&rout, OFF, NUM_ROUTS);
-               
-                        lum = Serial.parseInt(); //luminescence
-                        check_int(&lum, OFF, MIDI_MAX);
+                        col->r = Serial.parseInt(); //red
+                        check_int(&col->r, OFF, MIDI_MAX);
+                        
+                        col->g = Serial.parseInt(); //green
+                        check_int(&col->g, OFF, MIDI_MAX);
+                        
+                        col->b = Serial.parseInt(); //blue
+                        check_int(&col->b, OFF, MIDI_MAX);
                         
                         pix = Serial.parseInt(); //pixel
-                
+                        check_int(&pix, OFF, (NUM_LEDS - 1));
+                        
                         width = Serial.parseInt(); //width
                         check_int(&width, OFF, MIDI_MAX);
                         get_width(&width);
                 }
+                
                 off();
-                if (width) {
-                        switch (rout) {
-                        case OFF:
-                                off();
-                                break;
-                        case MONO:
-                                mono(pix, lum, width);
-                                break;
-                        case SPECTRUM:
-                                spectrum(pix, lum, width);
-                                break;
-                        case DIAD:
-                                diad(pix, lum, width);
-                                break;
-                        case TRIAD:
-                                triad(pix, lum, width);
-                                break;
-                        case TETRAD:
-                                tetrad(pix, lum, width);
-                                break;
-                        case OUTSIDER:
-                                outsider(pix, lum, width);
-                                break;
-                        case OUTSIDER_TRIAD:
-                                outsider_triad(pix, lum, width);
-                                break;
-                        case OUTSIDER_TETRAD:
-                                outsider_tetrad(pix, lum, width);
-                        }
-                }
+                if (width)
+                        set_col(col, pix, width);
+                                
                 strip.show();
         }
+        free(col);
 }
 
 void
-off(void)
+off()
 {
-      unsigned cnt;
-      
-      for (cnt = 0; cnt < NUM_LEDS; ++cnt)
-              strip.setPixelColor(cnt, OFF);
-}
-
-void
-set_pix(struct rgb *col, int pix)
-{
-        strip.setPixelColor(pix, strip.Color(col->r, col->g, col->b));
+	unsigned cnt;
         
+        for (cnt = 0; cnt < NUM_LEDS; ++cnt)
+                strip.setPixelColor(cnt, OFF);
 }
 
-
+void
+set_pix(struct rgb* col, int pix)
+{
+        strip.setPixelColor(pix, col->r, col->g, col->b);
+}
